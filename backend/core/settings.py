b8267614202716c5aee7ec7889746fd015b4f0fe
Raman Pal <monusharma44654@gmail.com>
2026-09-20 16:41:40 +0530

    Fix duplicate staticfiles app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
